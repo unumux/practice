902a25b3bd12ac152e6a85a776fefd4e5a6bd2b2
Ivan Town <itown@unum.com>
2017-08-24 17:15:11 -0400

    Add max-width to body and JS for mobile nav toggle

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,12 @@
 // Main JS should go here!
 // Include scripts using Browserify by doing:
 // var $ = require("jquery");
+
+
+document.querySelector('.willow-page-header__content-open').addEventListener("click", function( event ) {
+    document.querySelector('.willow-page-header').dataset.contentOpen = true; 
+}, false);
+
+document.querySelector('.willow-page-header__content-close').addEventListener("click", function( event ) {
+    document.querySelector('.willow-page-header').dataset.contentOpen = false;
+}, false);
